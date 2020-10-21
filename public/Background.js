f26db7b406a278e0a30c517b44eaeb7d273b7d76
Charming <charming@cronsystems.com>
2020-10-21 10:41:42 +0530

    Second Commit

--- public/Background.js
+++ public/Background.js
@@ -4,29 +4,25 @@
 const source = ["screen", "window"];
 let backgroundConnection;
 
-chrome.browserAction.onClicked.addListener(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        let activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {message: "Extension Clicked"});
-    });
-});
 
 chrome.runtime.onConnect.addListener((port) => {
     port.postMessage({message: "Port Connected"});
 
     port.onMessage.addListener((msg) => {
-        if(msg.message === "Content-SDP")
+        if(msg.message === "Initiate RTC")
         {
             backgroundConnection = new RTCPeerConnection();
-            backgroundConnection.setRemoteDescription(new RTCSessionDescription(msg.sdp));
             backgroundConnection.onicecandidate = (e) => {
                 if(e.candidate)
                 {
-                    console.log('onice background')
-                    port.postMessage({ message: "Background Ice-Candidate", candidate: e.candidate });
+                    port.postMessage({ message: "Ice", candidate: e.candidate })
                 }
             }
+            backgroundConnection.onconnectionstatechange = (e) => {
+                console.log(e);
+            }
             chrome.desktopCapture.chooseDesktopMedia(source, (streamID) => {
+                port.postMessage({message: `Waiting For Affirmation`});
                 navigator.mediaDevices.getUserMedia({
                     audio: false,
                     video: {
@@ -38,63 +34,35 @@ chrome.runtime.onConnect.addListener((port) => {
                         }
                     }
                 })
-                .then((stream) => {
-
-                    // let recorder = new MediaRecorder(stream, { mimeType: "video/webm; codecs=vp9"});
-                    // let recordedData = [];
-                    // recorder.start(1000);
-                    // recorder.ondataavailable = (e) => {
-                    //     if(e.data.size > 0)
-                    //     {
-                    //         recordedData.push(e.data);
-                    //     }
-                    // }
-
-                    // stream.oninactive = () => {
-                    //     if(recorder.state !== 'inactive')
-                	// 		recorder.stop();
-                	// 	let blob = new Blob(recordedData);
-                    //     recordedData = [];
-                    //     let vidURL = window.URL.createObjectURL(blob);
-                    //     port.postMessage({message: "Stream Stopped" });
-                    //     // window.open(vidURL);
-                	// 	let a = document.createElement('a');
-                	// 	document.body.appendChild(a);
-                	// 	a.style = 'display: none';
-                	// 	a.href = vidURL;
-                	// 	a.download = 'Screencapture.webm';
-                	// 	a.click();
-                    //     window.URL.revokeObjectURL(vidURL);
-                    // }
-
-                    stream.getTracks().forEach((track) => {
+                .then((stream) => {   
+                    
+                    stream.getVideoTracks().forEach((track) => {
                         backgroundConnection.addTrack(track, stream);
                     })
-                    backgroundConnection.createAnswer()
-                    .then((answer) => {
-                        backgroundConnection.setLocalDescription(answer);
-                    })
-                    .then(() => {
-                        port.postMessage({ message: "Background-SDP", sdp: backgroundConnection.localDescription })
-                    })
+
+                    backgroundConnection.createOffer({ offerToReceiveVideo: true })
+                    .then((offer) => {
+                        backgroundConnection.setLocalDescription(offer)
+                        .then(() => {
+                            console.log(backgroundConnection)
+                            port.postMessage({ message: "Background-SDP", sdp: backgroundConnection.localDescription })
+                        })
+                    })       
                 })
-            });
+            })
         }
 
-
-        if(msg.message === "Content Ice-Candidate")
+        if(msg.message === "Content-SDP")
         {
-            if(msg.candidate)
-            {
-                console.log('msg ice from content');
-                backgroundConnection.addIceCandidate(new RTCIceCandidate(msg.candidate));
-            }
-            
-            // if(msg.candidate)
-            //     backgroundConnection.addIceCandidate(new RTCIceCandidate(msg.candidate));
+            backgroundConnection.setRemoteDescription(new RTCSessionDescription(msg.sdp))
+            console.log(backgroundConnection);
         }
 
-
+        // if(msg.message === "Ice")
+        // {
+        //     backgroundConnection.addIceCandidate(new RTCIceCandidate(msg.candidate))
+        //     // console.log(backgroundConnection);
+        // }
     })
 })
 
--- src/ContentScript.js
+++ src/ContentScript.js
@@ -20,91 +20,84 @@ export class ContentScript extends Component {
     let vid = document.createElement("video");
     vid.srcObject = stream;
     vid.autoplay = true;
+    vid.style.border = '2px solid black';
+    vid.style.zIndex = '3000'
     document.body.appendChild(vid);
 
     tracks.track.onmute = () => {
       console.log('video muted');
-      // if(this.recorder.state !== 'inactive')
-      //   this.recorder.stop();
-      // let blob = new Blob(this.recordedData);
-      // this.recordedData = [];
-      // let url = URL.createObjectURL(blob);
-      // let a = document.createElement('a');
-      // document.body.appendChild(a);
-      // a.style = 'display: none';
-      // a.href = url;
-      // a.download = 'Screencapture.webm';
-      // a.click();
-      // setTimeout(() => {
-      //   document.body.removeChild(a);
-      //   window.URL.revokeObjectURL(url);
-      // }, 1000)
+      if(this.recorder.state !== 'inactive')
+        this.recorder.stop();
+      let blob = new Blob(this.recordedData);
+      this.recordedData = [];
+      let url = URL.createObjectURL(blob);
+      let a = document.createElement('a');
+      document.body.appendChild(a);
+      a.style = 'display: none';
+      a.href = url;
+      a.download = 'Screencapture.webm';
+      a.click();
+      setTimeout(() => {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+      }, 1000)
     }
 
     tracks.track.onunmute = () => {
       console.log('video unmuted');
-      // this.recorder = new MediaRecorder(stream, { mimeType: "video/webm; codecs=vp8"});
-      // this.recorder.start(1000);
-      // this.recorder.ondataavailable = (e) => {
-      //   if(e.data.size > 0)
-      //   {
-      //     // this.recordedData.push(e.data);
-      //     console.log(e.data);
-      //   }
-      // }
+      this.recorder = new MediaRecorder(stream, { mimeType: "video/webm; codecs=vp8"});
+      this.recorder.start(1000);
+      this.recorder.ondataavailable = (e) => {
+        if(e.data.size > 0)
+        {
+          this.recordedData.push(e.data);
+          console.log(e.data);
+        }
+      }
     }
   }
 
-  handleDataChannel = (e) => {
-    console.log(e);
+  handleIceCandidate = (e) => {
+    if(e.candidate)
+    {
+      this.port.postMessage({ message: "Ice", candidate: e.candidate });
+    }
   }
 
   componentDidMount = () => {
-    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-      console.log(request.message);
-    });
 
-    this.port.onMessage.addListener((request) => { 
-      if(request.message === "Port Connected")
+    this.port.onMessage.addListener((msg) => { 
+      if(msg.message === "Background-SDP")
       {
-        console.log(request.message);
-      }
-      if(request.message === "Background-SDP")
-      {
-        this.contentConnection.setRemoteDescription(new RTCSessionDescription(request.sdp));
-      }
-      if(request.message === "Background Ice-Candidate")
-      {
-        if(request.candidate)
-        {
-          this.contentConnection.addIceCandidate(new RTCIceCandidate(request.candidate));
+        this.contentConnection = new RTCPeerConnection();
+        this.contentConnection.ontrack = this.handleTracks;
+        this.contentConnection.onicecandidate = this.handleIceCandidate;
+        this.contentConnection.oniceconnectionstatechange = (e) => {
+          console.log(e)
         }
+        this.contentConnection.setRemoteDescription(new RTCSessionDescription(msg.sdp))
+        .then(() => {
+          this.contentConnection.createAnswer()
+          .then((answer) => {
+            this.contentConnection.setLocalDescription(answer)
+            .then(() => {
+              this.port.postMessage({ message: "Content-SDP", sdp: this.contentConnection.localDescription })
+              console.log(this.contentConnection);
+            })
+          })
+        })
       }
 
+      // if(msg.message === "Ice")
+      // {
+      //   // this.contentConnection.addIceCandidate(new RTCIceCandidate(msg.candidate));
+      // }
     })
 
 
     setTimeout(() => {
-      this.contentConnection = new RTCPeerConnection();
-      this.contentConnection.ontrack = this.handleTracks;
-      this.contentConnection.onicecandidate = (e) => {
-        if(e.candidate)
-        {
-          this.port.postMessage({ message: "Content Ice-Candidate", candidate: e.candidate });
-        }
-      }
-      this.contentConnection.createOffer({ offerToReceiveVideo: true })
-      .then((offer) => {
-        this.contentConnection.setLocalDescription(offer);
-      })
-      .then(() => {
-        this.port.postMessage({ 
-          message: "Content-SDP", 
-          sdp: this.contentConnection.localDescription
-        })
-      })
-    }, 5000)
-
+      this.port.postMessage({ message: "Initiate RTC" });
+    }, 3000)
   }
 
 
