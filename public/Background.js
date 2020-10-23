8a53ac9e69cc608bb3173b68ae1ee37acd79945b
Charming <charming@cronsystems.com>
2020-10-23 07:01:40 +0530

    Fully functioning webrtc video streaming between Background and Content Script

--- public/Background.js
+++ public/Background.js
@@ -1,69 +1,64 @@
 /* global chrome */
-
-
-const source = ["screen", "window"];
-let backgroundConnection;
-
+const source = ["window", "screen"];
+let backgroundPeer;
+let iceCandidates = [];
 
 chrome.runtime.onConnect.addListener((port) => {
-    port.postMessage({message: "Port Connected"});
+
+    port.postMessage({ message: "Port Connected" })
 
     port.onMessage.addListener((msg) => {
-        if(msg.message === "Initiate RTC")
-        {
-            backgroundConnection = new RTCPeerConnection();
-            backgroundConnection.onicecandidate = (e) => {
-                if(e.candidate)
-                {
-                    port.postMessage({ message: "Ice", candidate: e.candidate })
-                }
-            }
-            backgroundConnection.onconnectionstatechange = (e) => {
-                console.log(e);
-            }
-            chrome.desktopCapture.chooseDesktopMedia(source, (streamID) => {
-                port.postMessage({message: `Waiting For Affirmation`});
-                navigator.mediaDevices.getUserMedia({
-                    audio: false,
-                    video: {
-                        mandatory: {
-                            chromeMediaSource: 'desktop',
-                            chromeMediaSourceId: streamID,
-                            maxWidth: window.screen.width,
-                            maxHeight: window.screen.height
-                        }
-                    }
-                })
-                .then((stream) => {   
-                    
-                    stream.getVideoTracks().forEach((track) => {
-                        backgroundConnection.addTrack(track, stream);
-                    })
-
-                    backgroundConnection.createOffer({ offerToReceiveVideo: true })
-                    .then((offer) => {
-                        backgroundConnection.setLocalDescription(offer)
-                        .then(() => {
-                            console.log(backgroundConnection)
-                            port.postMessage({ message: "Background-SDP", sdp: backgroundConnection.localDescription })
-                        })
-                    })       
-                })
-            })
-        }
-
         if(msg.message === "Content-SDP")
         {
-            backgroundConnection.setRemoteDescription(new RTCSessionDescription(msg.sdp))
-            console.log(backgroundConnection);
+            initiateRTC(msg.sdp);
         }
 
-        // if(msg.message === "Ice")
-        // {
-        //     backgroundConnection.addIceCandidate(new RTCIceCandidate(msg.candidate))
-        //     // console.log(backgroundConnection);
-        // }
+        if(msg.message === "Ice")
+        {
+            iceCandidates.push(msg.candidate);
+        }
     })
+
+    async function initiateRTC(sdp) {
+        backgroundPeer = new RTCPeerConnection();
+        backgroundPeer.onicecandidate = handleIceCandidate;
+        await backgroundPeer.setRemoteDescription(new RTCSessionDescription(sdp));
+        chrome.desktopCapture.chooseDesktopMedia(source, (streamID) => {
+            navigator.mediaDevices.getUserMedia({
+                audio: false,
+                video: {
+                    mandatory: {
+                        chromeMediaSource: 'desktop',
+                        chromeMediaSourceId: streamID,
+                        maxWidth: window.screen.width,
+                        maxHeight: window.screen.height
+                    }
+                }
+            })
+            .then(async (stream) => {
+                stream.getVideoTracks().forEach((track) => {
+                    backgroundPeer.addTrack(track, stream);
+                })
+                let answer = await backgroundPeer.createAnswer();
+                await backgroundPeer.setLocalDescription(answer);
+                port.postMessage({ message: "Background-SDP", sdp: backgroundPeer.localDescription });
+                addCandidates()
+            })
+            .catch((err) => {
+                console.log(`ERROR WHILE CAPTURING STREAM: ${err}`);
+            })
+        })
+    }
+
+
+    function handleIceCandidate(e) {
+        if(e.candidate)
+            port.postMessage({ message: "Ice", candidate: e.candidate })
+    }
+
+    function addCandidates() {
+        iceCandidates.forEach((candidate) => {
+            backgroundPeer.addIceCandidate(new RTCIceCandidate(candidate));
+        })
+    }
 })
-
-
--- src/ContentScript.js
+++ src/ContentScript.js
@@ -1,105 +1,105 @@
 import { Component } from 'react';
 
-/* 
-  global chrome
- */
+/* global chrome */
 
 export class ContentScript extends Component {
   constructor() {
     super();
-    this.port = chrome.runtime.connect(null, { name: "RTC-Port" });
-    this.contentConnection = undefined;
-    this.recorder = undefined;
-    this.recordedData = [];
+    this.contentPeer = undefined;
+    this.port = chrome.runtime.connect(null, { name: "Connect" });
+    this.iceCandidates = [];
   }
 
-
-  handleTracks = (tracks) => {
-    console.log(tracks);
-    let stream = tracks.streams[0];
-    let vid = document.createElement("video");
-    vid.srcObject = stream;
-    vid.autoplay = true;
-    vid.style.border = '2px solid black';
-    vid.style.zIndex = '3000'
-    document.body.appendChild(vid);
-
-    tracks.track.onmute = () => {
-      console.log('video muted');
-      if(this.recorder.state !== 'inactive')
-        this.recorder.stop();
-      let blob = new Blob(this.recordedData);
-      this.recordedData = [];
-      let url = URL.createObjectURL(blob);
+  handleTracks = (e) => {
+    let stream = e.streams[0];
+    let recorder = new MediaRecorder(stream, { mimeType: "video/webm; codecs=vp9"});
+    let recordedData = [];
+    e.track.onmute = () => {
+      console.log('Track muted');
+      if(recorder.state !== 'inactive')
+        recorder.stop();
+      let blob = new Blob(recordedData);
+      recordedData = [];
+      let vidURL = window.URL.createObjectURL(blob);
       let a = document.createElement('a');
       document.body.appendChild(a);
       a.style = 'display: none';
-      a.href = url;
+      a.href = vidURL;
       a.download = 'Screencapture.webm';
       a.click();
       setTimeout(() => {
         document.body.removeChild(a);
-        window.URL.revokeObjectURL(url);
+        window.URL.revokeObjectURL(vidURL);
       }, 1000)
     }
 
-    tracks.track.onunmute = () => {
-      console.log('video unmuted');
-      this.recorder = new MediaRecorder(stream, { mimeType: "video/webm; codecs=vp8"});
-      this.recorder.start(1000);
-      this.recorder.ondataavailable = (e) => {
+    e.track.onunmute = () => {
+      console.log('Track unmuted');
+      recorder.start(500);
+      recorder.ondataavailable = (e) => {
         if(e.data.size > 0)
         {
-          this.recordedData.push(e.data);
-          console.log(e.data);
+          recordedData.push(e.data);
         }
       }
     }
   }
 
-  handleIceCandidate = (e) => {
+
+  handleIceCandidates = (e) => {
     if(e.candidate)
-    {
-      this.port.postMessage({ message: "Ice", candidate: e.candidate });
-    }
+      this.port.postMessage({ message: "Ice", candidate: e.candidate })
+  }
+
+
+  initiateRTC =  async () => {
+    this.contentPeer = new RTCPeerConnection();
+    this.contentPeer.ontrack = this.handleTracks;
+    this.contentPeer.onicecandidate = this.handleIceCandidates;
+    let offer = await this.contentPeer.createOffer({ offerToReceiveVideo: true });
+    await this.contentPeer.setLocalDescription(offer);
+    this.port.postMessage({ message: "Content-SDP", sdp: this.contentPeer.localDescription });
   }
 
   componentDidMount = () => {
+    this.port.onMessage.addListener((msg) => {
+      if(msg.message === "Port Connected")
+      {
+        console.log(msg.message);
+      }
 
-    this.port.onMessage.addListener((msg) => { 
       if(msg.message === "Background-SDP")
       {
-        this.contentConnection = new RTCPeerConnection();
-        this.contentConnection.ontrack = this.handleTracks;
-        this.contentConnection.onicecandidate = this.handleIceCandidate;
-        this.contentConnection.oniceconnectionstatechange = (e) => {
-          console.log(e)
-        }
-        this.contentConnection.setRemoteDescription(new RTCSessionDescription(msg.sdp))
-        .then(() => {
-          this.contentConnection.createAnswer()
-          .then((answer) => {
-            this.contentConnection.setLocalDescription(answer)
-            .then(() => {
-              this.port.postMessage({ message: "Content-SDP", sdp: this.contentConnection.localDescription })
-              console.log(this.contentConnection);
-            })
-          })
+        this.contentPeer.setRemoteDescription(new RTCSessionDescription(msg.sdp));
+        this.iceCandidates.forEach((candidate) => {
+          this.contentPeer.addIceCandidate(new RTCIceCandidate(candidate));
         })
       }
 
-      // if(msg.message === "Ice")
-      // {
-      //   // this.contentConnection.addIceCandidate(new RTCIceCandidate(msg.candidate));
-      // }
+      if(msg.message === "Ice")
+      {
+        this.iceCandidates.push(msg.candidate);
+      }
+
     })
 
 
+
+    //initiate RTC connection from content script after two second from when the extension loads
     setTimeout(() => {
-      this.port.postMessage({ message: "Initiate RTC" });
+      this.initiateRTC()
+    }, 2000)
+
+    setInterval(() => {
+      if(this.contentPeer)
+      {
+        if(this.contentPeer.connectionState === "connected")
+        {
+          console.log('Content Peer Connected');
+        }
+      }
     }, 3000)
   }
-
 
   render = () => {
     return null;
